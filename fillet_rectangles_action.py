e12dd0a699915df2bc105bc45a2fea73f67748c4
Siddharth Kothari <siddkot@gmail.com>
2022-12-15 08:44:56 +0530

    fixed crashing bug

--- fillet_rectangles_action.py
+++ fillet_rectangles_action.py
@@ -4,33 +4,32 @@ import wx
 from .fillet_rectangles_gui import RadiusInputDialog
 
 
-def add_line(start, end, layer=pcbnew.Edge_Cuts):
-    board = pcbnew.GetBoard()
-    segment = pcbnew.PCB_SHAPE(board)
-    segment.SetShape(pcbnew.SHAPE_T_SEGMENT)
-    segment.SetStart(start)
-    segment.SetEnd(end)
-    segment.SetLayer(layer)
-    segment.SetWidth(int(0.1 * pcbnew.IU_PER_MM))
-    board.Add(segment)
-
-
-def add_line_arc(start, center, angle=90, layer=pcbnew.Edge_Cuts):
-    board = pcbnew.GetBoard()
-    arc = pcbnew.PCB_SHAPE(board)
-    arc.SetShape(pcbnew.SHAPE_T_ARC)
-    arc.SetStart(start)
-    arc.SetCenter(center)
-    arc.SetArcAngleAndEnd(angle * 10, False)
-    arc.SetLayer(layer)
-    arc.SetWidth(int(0.1 * pcbnew.IU_PER_MM))
-    board.Add(arc)
-
 class FilletRectangles(RadiusInputDialog):
     def __init__(self, board, action):
         super(FilletRectangles, self).__init__(None)
         self.board = board
         self.action = action
+
+    def add_line(self, start, end, layer=pcbnew.Edge_Cuts):
+
+        segment = pcbnew.PCB_SHAPE(self.board)
+        segment.SetShape(pcbnew.SHAPE_T_SEGMENT)
+        segment.SetStart(start)
+        segment.SetEnd(end)
+        segment.SetLayer(layer)
+        segment.SetWidth(int(0.1 * pcbnew.IU_PER_MM))
+        self.board.Add(segment)
+
+
+    def add_line_arc(self, start, center, angle=90, layer=pcbnew.Edge_Cuts):
+        arc = pcbnew.PCB_SHAPE(self.board)
+        arc.SetShape(pcbnew.SHAPE_T_ARC)
+        arc.SetStart(start)
+        arc.SetCenter(center)
+        arc.SetArcAngleAndEnd(angle * 10, False)
+        arc.SetLayer(layer)
+        arc.SetWidth(int(0.1 * pcbnew.IU_PER_MM))
+        self.board.Add(arc)
 
     def RadiusInputDialogOnClose( self, event ):
         self.EndModal(wx.ID_OK)
@@ -38,28 +37,28 @@ class FilletRectangles(RadiusInputDialog):
     def runScript( self, event ):
         radius = int(self.t1.GetLineText(0))
 
-        board = pcbnew.GetBoard()
         number_of_selected_rectangles = 0
-        for drw in board.GetDrawings():
+        for drw in self.board.GetDrawings():
             if(drw.IsSelected() and str(drw.SHAPE_T_asString()) == "S_RECT"):
 
                 try:
+
                     number_of_selected_rectangles +=1
                     rectData = drw.GetRectCorners()
                     radius_x_offset = pcbnew.wxPoint(radius * pcbnew.IU_PER_MM, 0)
                     radius_y_offset = pcbnew.wxPoint(0, radius * pcbnew.IU_PER_MM)
-                    add_line(rectData[0] + radius_x_offset, rectData[1] - radius_x_offset) # Top left - Top right
-                    add_line(rectData[1] + radius_y_offset, rectData[2] - radius_y_offset) # Top Right - bottom right
-                    add_line(rectData[2] - radius_x_offset, rectData[3] + radius_x_offset) # bottom right - bottom left
-                    add_line(rectData[3] - radius_y_offset, rectData[0] + radius_y_offset) # bottom left - top left
-                    add_line_arc(rectData[0] + radius_y_offset, rectData[0] + radius_y_offset + radius_x_offset, angle=90) # top left
-                    add_line_arc(rectData[1] - radius_x_offset, rectData[1] - radius_x_offset + radius_y_offset, angle=90) # top right
-                    add_line_arc(rectData[2] - radius_y_offset, rectData[2] - radius_y_offset - radius_x_offset, angle=90) # bottom right
-                    add_line_arc(rectData[3] + radius_x_offset, rectData[3] + radius_x_offset - radius_y_offset, angle=90) # bottom left
-                    board.Delete(drw)
+                    self.add_line(rectData[0] + radius_x_offset, rectData[1] - radius_x_offset) # Top left - Top right
+                    self.add_line(rectData[1] + radius_y_offset, rectData[2] - radius_y_offset) # Top Right - bottom right
+                    self.add_line(rectData[2] - radius_x_offset, rectData[3] + radius_x_offset) # bottom right - bottom left
+                    self.add_line(rectData[3] - radius_y_offset, rectData[0] + radius_y_offset) # bottom left - top left
+                    self.add_line_arc(rectData[0] + radius_y_offset, rectData[0] + radius_y_offset + radius_x_offset, angle=90) # top left
+                    self.add_line_arc(rectData[1] - radius_x_offset, rectData[1] - radius_x_offset + radius_y_offset, angle=90) # top right
+                    self.add_line_arc(rectData[2] - radius_y_offset, rectData[2] - radius_y_offset - radius_x_offset, angle=90) # bottom right
+                    self.add_line_arc(rectData[3] + radius_x_offset, rectData[3] + radius_x_offset - radius_y_offset, angle=90) # bottom left
+                    self.board.RemoveNative(drw)
                     self.EndModal(wx.ID_OK)
                 except Exception as e:
-                    wx.MessageBox(e, 'Info',  wx.OK | wx.ICON_INFORMATION)
+                    wx.MessageBox(str(e), 'Info',  wx.OK | wx.ICON_INFORMATION)
         if number_of_selected_rectangles == 0:
             wx.MessageBox("Please select a rectangle", 'Info',  wx.OK | wx.ICON_INFORMATION)
             self.EndModal(wx.ID_OK)
