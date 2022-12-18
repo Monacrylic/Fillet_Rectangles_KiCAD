ac2a3d68d464a54e0dd5f2abb3408ca2db3ed644
Siddharth Kothari <siddkot@gmail.com>
2022-12-18 11:39:20 +0530

    maintains lineWidth

--- fillet_rectangles_action.py
+++ fillet_rectangles_action.py
@@ -18,25 +18,25 @@ class FilletRectangles(RadiusInputDialog):
         self.board = board
         self.action = action
 
-    def add_line(self, start, end, layer=pcbnew.Edge_Cuts):
+    def add_line(self, start, end, layer=pcbnew.Edge_Cuts, line_width = 0.1*IU_PER_MM):
 
         segment = pcbnew.PCB_SHAPE(self.board)
         segment.SetShape(pcbnew.SHAPE_T_SEGMENT)
         segment.SetStart(start)
         segment.SetEnd(end)
         segment.SetLayer(layer)
-        segment.SetWidth(int(0.1 * IU_PER_MM))
+        segment.SetWidth(int(line_width))
         self.board.Add(segment)
 
 
-    def add_line_arc(self, start, center, angle=90, layer=pcbnew.Edge_Cuts):
+    def add_line_arc(self, start, center, angle=90, layer=pcbnew.Edge_Cuts, line_width = 0.1*IU_PER_MM):
         arc = pcbnew.PCB_SHAPE(self.board)
         arc.SetShape(pcbnew.SHAPE_T_ARC)
         arc.SetStart(start)
         arc.SetCenter(center)
         arc.SetArcAngleAndEnd(angle * 10, False)
         arc.SetLayer(layer)
-        arc.SetWidth(int(0.1 * IU_PER_MM))
+        arc.SetWidth(int(line_width))
         self.board.Add(arc)
 
     def RadiusInputDialogOnClose( self, event ):
@@ -51,6 +51,7 @@ class FilletRectangles(RadiusInputDialog):
 
                 try:
                     layer = drw.GetLayer()
+                    line_width = drw.GetWidth()
                     number_of_selected_rectangles +=1
                     unorganized_rect_data = drw.GetRectCorners()
                     x_array = []
@@ -68,14 +69,14 @@ class FilletRectangles(RadiusInputDialog):
 
                     radius_x_offset = pcbnew.wxPoint(radius * IU_PER_MM, 0)
                     radius_y_offset = pcbnew.wxPoint(0, radius * IU_PER_MM)
-                    self.add_line(rectData[0] + radius_x_offset, rectData[1] - radius_x_offset, layer) # Top left - Top right
-                    self.add_line(rectData[1] + radius_y_offset, rectData[2] - radius_y_offset, layer) # Top Right - bottom right
-                    self.add_line(rectData[2] - radius_x_offset, rectData[3] + radius_x_offset, layer) # bottom right - bottom left
-                    self.add_line(rectData[3] - radius_y_offset, rectData[0] + radius_y_offset, layer) # bottom left - top left
-                    self.add_line_arc(rectData[0] + radius_y_offset, rectData[0] + radius_y_offset + radius_x_offset, angle=90, layer=layer) # top left
-                    self.add_line_arc(rectData[1] - radius_x_offset, rectData[1] - radius_x_offset + radius_y_offset, angle=90, layer=layer) # top right
-                    self.add_line_arc(rectData[2] - radius_y_offset, rectData[2] - radius_y_offset - radius_x_offset, angle=90, layer=layer) # bottom right
-                    self.add_line_arc(rectData[3] + radius_x_offset, rectData[3] + radius_x_offset - radius_y_offset, angle=90, layer=layer) # bottom left
+                    self.add_line(rectData[0] + radius_x_offset, rectData[1] - radius_x_offset, layer, line_width = line_width) # Top left - Top right
+                    self.add_line(rectData[1] + radius_y_offset, rectData[2] - radius_y_offset, layer, line_width = line_width) # Top Right - bottom right
+                    self.add_line(rectData[2] - radius_x_offset, rectData[3] + radius_x_offset, layer, line_width = line_width) # bottom right - bottom left
+                    self.add_line(rectData[3] - radius_y_offset, rectData[0] + radius_y_offset, layer, line_width = line_width) # bottom left - top left
+                    self.add_line_arc(rectData[0] + radius_y_offset, rectData[0] + radius_y_offset + radius_x_offset, angle=90, layer=layer, line_width = line_width) # top left
+                    self.add_line_arc(rectData[1] - radius_x_offset, rectData[1] - radius_x_offset + radius_y_offset, angle=90, layer=layer, line_width = line_width) # top right
+                    self.add_line_arc(rectData[2] - radius_y_offset, rectData[2] - radius_y_offset - radius_x_offset, angle=90, layer=layer, line_width = line_width) # bottom right
+                    self.add_line_arc(rectData[3] + radius_x_offset, rectData[3] + radius_x_offset - radius_y_offset, angle=90, layer=layer, line_width = line_width) # bottom left
                     self.board.RemoveNative(drw)
                     self.EndModal(wx.ID_OK)
                 except Exception as e:
